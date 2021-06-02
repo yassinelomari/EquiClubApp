3077ce1eeb4f7bf5a81e6252c3192dcc8952e379
ML-Soufi <soufimohamedlarbi.professional@gmail.com>
2021-06-02 08:53:47 +0100

    first integration MLS-13

--- app/src/main/java/com/example/equiclubapp/MonitorActivity.java
+++ app/src/main/java/com/example/equiclubapp/MonitorActivity.java
@@ -3,24 +3,17 @@ package com.example.equiclubapp;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import org.naishadhparmar.zcustomcalendar.CustomCalendar;
+public class MonitorActivity extends AppCompatActivity {
 
-public class CalendarActivity extends AppCompatActivity {
-    ImageView today, week, month, year;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
-        setContentView(R.layout.activity_calendar);
-        today = findViewById(R.id.today);
-        week = findViewById(R.id.week);
-        month = findViewById(R.id.month);
-        year = findViewById(R.id.year);
+        setContentView(R.layout.activity_monitor);
     }
 }
